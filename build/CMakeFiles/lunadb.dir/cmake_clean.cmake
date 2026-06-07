cd98be8883c8130efd5a16b95c8c86f209361969
Ciro Sánchez <ciroandressanchezbuitrago@gmail.com>
2026-06-07 18:49:37 -0500

    init: first commit

---
+++ build/CMakeFiles/lunadb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lunadb.dir/link.d"
+  "CMakeFiles/lunadb.dir/src/main.cpp.o"
+  "CMakeFiles/lunadb.dir/src/main.cpp.o.d"
+  "lunadb"
+  "lunadb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lunadb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/lunadb.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/lunadb.dir/src/main.cpp.o: \
+ /home/csanchez/Projects/personal/lunadb/src/main.cpp \
+ /usr/include/stdc-predef.h
