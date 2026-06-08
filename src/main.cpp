f230697686a56b03586b131a7f8e71156d7605d3
Ciro Sánchez <ciroandressanchezbuitrago@gmail.com>
2026-06-07 23:02:25 -0500

    feat: new main class to test the new lunadb::Database class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include "../include/db.hpp"
+
 int main() {
-    
+    std::cout << "This is LunaDB" << std::endl;
+    lunadb::Database db;
+
+    db.set("artist", "catriel y pacoh");
+
+    db.set("language", "cpp");
+
+    std::cout << db.get("artist") << std::endl;
+
+    std::cout << db.get("not an artist") << std::endl;
+
     return 0;
 }
